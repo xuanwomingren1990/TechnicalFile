17bd0730b8b9884aea6655074a757e8c7f27bd6e
xuanwomingren1989 <798252480@qq.com>
2021-01-16 17:11:09 +0800

    添加坐标反算公式

--- src/util/App.vue
+++ src/util/App.vue
@@ -1,36 +1,68 @@
 <template>
   <div>
     <div>
-      <span>B={{B}}</span>
-      <span style="display: inline-block;width: 20px;"></span>
-      <span>L={{L}}</span>
+      <h4>坐标正算：</h4>
+      <div>
+        <span style="display: inline-block;width: 300px;">维度B={{B}}</span>
+        <span style="display: inline-block;width: 300px;">经度L={{L}}</span>
+      </div>
+      <div>
+        <span style="display: inline-block;width: 300px;">横坐标x={{XY.x}}</span>
+        <span style="display: inline-block;width: 300px;">纵坐标y={{XY.y}}</span>
+      </div>
     </div>
+
     <div>
-      <span>x={{result.x}}</span>
-      <span style="display: inline-block;width: 20px;"></span>
-      <span>y={{result.y}}</span>
+      <h4>坐标反算：</h4>
+      <div>
+        <span style="display: inline-block;width: 300px;">横坐标x={{x}}</span>
+        <span style="display: inline-block;width: 300px;">纵坐标y={{y}}</span>
+      </div>
+      <div>
+        维度:
+        <span>{{BL.B.D}}度</span>
+        <span style="margin: 0 20px">{{BL.B.M}}分</span>
+        <span>{{BL.B.S}}秒</span>
+      </div>
+      <div>
+        经线度:
+        <span>{{BL.L.D}}度</span>
+        <span style="margin: 0 20px">{{BL.L.M}}分</span>
+        <span>{{BL.L.S}}秒</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import {BL2XY} from './util'
+import {BL2XY,XY2BL} from './util'
+
 export default {
 name: "App",
   data(){
     return{
+      // 坐标正算原始经纬度
       B:30.45,
-      L:114.45
+      L:114.45,
+
+      // 坐标反算原始坐标
+      x:700000,
+      y:4000000
     }
   },
   computed:{
-    result(){
-      return BL2XY(this.B,this.L,114)
+    // 坐标正算结果
+    XY(){
+      return BL2XY(this.B, this.L)
+    },
+    // 坐标反算结果
+    BL(){
+      return XY2BL(this.x,this.y)
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="sass">
 
 </style>
--- src/util/util.js
+++ src/util/util.js
@@ -1,6 +1,8 @@
 /**
  * 对象深拷贝
  */
+// import {parse} from "ol/xml";
+
 export const deepClone = data => {
     var type = getObjType(data);
     var obj;
@@ -45,15 +47,18 @@ export const getObjType = obj => {
 
 
 /**
- * 坐标正算
- * @param B
- * @param L
- * @param L0    中央经线
- * @returns {{x: number, y: number}}
+ * WKID4547投影坐标系正算公式
+ * 椭球:         CGCS2000
+ * 投影方法:      高斯克吕格投影
+ * 中央经线:      114E
+ * @param B     纬度
+ * @param L     经度
+ * @returns {{x: number, y: number}} x为赤道方向坐标，y为中央经线方向坐标
  * @constructor
  */
-export const BL2XY = (B,L,L0)=>{ // eslint-disable-line no-unused-vars
-    const B_Rad = B / 180 * Math.PI // eslint-disable-line no-unused-vars // 弧度单位
+export const BL2XY = (B,L)=>{ // eslint-disable-line no-unused-vars
+    const L0 = 114
+    const B_Rad = B / 180 * Math.PI // 弧度单位
     const l_Rad = (L - L0) / 180 * Math.PI // 经度差，弧度单位
 
     const a = 6378137         //长轴
@@ -93,19 +98,121 @@ export const BL2XY = (B,L,L0)=>{ // eslint-disable-line no-unused-vars
     const X = a0 * B_Rad - (1/2) * a2 * Math.sin(2 * B_Rad) + (1 / 4) * a4 * Math.sin(4 * B_Rad) - (1 / 6) * a6 * Math.sin(6 * B_Rad) + (1 / 8) * a8 * Math.sin(8 * B_Rad)
 
     const Bl = Math.cos(B_Rad) * l_Rad
+
+    // 中央经线投影为纵轴(x)
     var x = X +
             (1 / 2) * N * t * Math.pow(Bl,2) +
             (1 / 24) * N * t * (5 - Math.pow(t,2) + 9 * Math.pow(ŋ,2) + 4 * Math.pow(ŋ,4)) * Math.pow(Bl,4) +
             (1 / 720) * N * t * (61 - 58 * Math.pow(t,2) + Math.pow(t,4)) * Math.pow(Bl,6)
 
-    var y = N * Bl +
+    // 赤道投影为横轴(y)
+    // 坐标纵轴西移500公里当作起始轴
+    var y = 500000 +
+            N * Bl +
             (1/6) * N * (1 - Math.pow(t,2) + Math.pow(ŋ,2)) * Math.pow(Bl,3) +
             (1/120) * N * (5 - 18 * Math.pow(t,2) + Math.pow(t,4) + 14 * Math.pow(ŋ,2) - 58 * Math.pow(t,2) * Math.pow(ŋ,2) ) * Math.pow(Bl,5)
 
 
 
     return {
-        x: y + 500000,
+        x: y,
         y: x
     }
 }
+
+
+export const XY2BL = (xx,yy)=>{ // eslint-disable-line no-unused-vars
+    var x = yy
+    var y = xx - 500000
+    const L0 = 114
+    const a = 6378137         //长轴
+    const f = 1 / 298.257222101   //扁率
+    const b = a - a * f       //短轴。公式:f= (a-b)/a
+    const e = Math.sqrt(a*a - b*b) / a //第一偏心率
+    const eʹ = Math.sqrt(a*a - b*b) / b // 第二偏心率
+
+
+    const m0 = a * (1 - e * e)
+    const m2 = (3 / 2) * e * e * m0
+    const m4 = (5 / 4) * e * e * m2
+    const m6 = (7 / 6) * e * e * m4
+    const m8 = (9 / 8) * e * e * m6
+
+    const n0 =  a
+    const n2 = (1 / 2) * e * e * n0
+    const n4 = (3 / 4) * e * e * n2
+    const n6 = (5 / 6) * e * e * n4
+    const n8 = (7 / 8) * e * e * n6 // eslint-disable-line no-unused-vars
+
+    const a0 = m0 + (1/2) * m2 + (3/8) * m4 + (5/16) * m6 + (35/128) * m8
+    const a2 = (1/2) * m2 + (1/2) * m4 + (15/32) * m6 + (7/16) * m8
+    const a4 = (1/8) * m4 + (3/16) * m6 + (7/32) * m8
+    const a6 = (1/32) * m6 + (1/16) * m8
+    const a8 = (1/128) * m8
+
+    var B0 = x / a0
+    const getB0 = ()=> {
+        var B1 = (x + a2 * (1/2) * Math.sin(2 * B0) - a4 * (1/4)  * Math.sin(4 * B0) + a6 * (1/6)  * Math.sin(6 * B0) - a8 * (1/8)  * Math.sin(8 * B0)) / a0
+
+        if ((B1 - B0) < 2.78 * Math.pow(10,-9)){
+            B0 = B1
+            return
+        }else {
+            B0 = B1
+            getB0()
+        }
+    }
+    getB0()
+
+    var sinB0 = Math.sin(B0)
+    var t = Math.tan(B0)
+    var ŋ2 = eʹ * Math.pow(Math.cos(B0),2)
+    var V = Math.sqrt(1 + Math.pow(eʹ,2) * Math.pow(Math.cos(B0),2))
+    var N = n0 + n2 * Math.pow(sinB0,2) + n4 * Math.pow(sinB0,4) + n6 * Math.pow(sinB0,6) + n8 * Math.pow(sinB0,8)
+
+    var B = B0 - (1 / 2) * Math.pow(V,2) * t * [
+        Math.pow((y / N),2) -
+        (1/12) * (5 + 3 * Math.pow(t,2) + ŋ2 - 9 * ŋ2 * Math.pow(t,2)) * Math.pow((y / N),4) +
+        (1/360) * (61 + 90 * Math.pow(t,2) + 45 * Math.pow(t,4)) * Math.pow((y/N),6)
+    ]
+
+    var l = (1 / Math.cos(B0)) * [
+        (y / N) -
+        (1 / 6) * (1 + 2 * Math.pow(t,2) + ŋ2) * Math.pow((y/N),3) +
+        (1 / 120) * (5 + 28 * Math.pow(t,2) + 24 * Math.pow(t,4) + 6 * ŋ2 + 8 * ŋ2 * Math.pow(t,2)) * Math.pow((y/N),5)
+    ]
+
+
+
+    return {
+        B:R2DMS(B),
+        L:R2DMS(L0 / 180 * Math.PI + l)
+    }
+
+}
+
+/**
+ * 弧度转度分秒
+ * @param rad       角度，以弧度为单位
+ * @constructor
+ */
+function R2DMS (rad){
+    var DD = rad  / Math.PI * 180
+    var D = parseInt(DD)
+    var D_spare = DD - D
+
+    var MM = D_spare * 60
+    var M = parseInt(MM)
+    var M_spare = MM - M
+
+    var S = M_spare * 60
+
+    return {
+        DD:DD,
+        D:D,
+        M:M,
+        S:S
+    }
+}
+
+
