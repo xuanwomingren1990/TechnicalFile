690e0e043422cbb7756bcfcd8e3e5fbfffab5841
xuanwomingren1989 <798252480@qq.com>
2021-01-18 10:39:07 +0800

    添加备注

--- src/util/util.js
+++ src/util/util.js
@@ -47,16 +47,16 @@ export const getObjType = obj => {
 
 
 /**
- * WKID4547投影坐标系正算公式
+ * WKID:4547投影坐标系正算公式
  * 椭球:         CGCS2000
- * 投影方法:      高斯克吕格投影
+ * 投影方法:      高斯-克吕格，3度带投影
  * 中央经线:      114E
  * @param B     纬度
  * @param L     经度
  * @returns {{x: number, y: number}} x为赤道方向坐标，y为中央经线方向坐标
  * @constructor
  */
-export const BL2XY = (B,L)=>{ // eslint-disable-line no-unused-vars
+export const BL2XY = (B,L)=>{
     const L0 = 114
     const B_Rad = B / 180 * Math.PI // 弧度单位
     const l_Rad = (L - L0) / 180 * Math.PI // 经度差，弧度单位
@@ -68,9 +68,28 @@ export const BL2XY = (B,L)=>{ // eslint-disable-line no-unused-vars
     const eʹ = Math.sqrt(a*a - b*b) / b //第二偏心率
 
     const t = Math.tan(B_Rad)
-    const ŋ = eʹ * Math.cos(B_Rad)
+    const t2 = Math.pow(t,2)
+    const t4 = Math.pow(t,4)
 
-    /*----------------------------------------------*/
+    const ŋ = eʹ * Math.cos(B_Rad)
+    const ŋ2 = Math.pow(ŋ,2)
+    const ŋ4 = Math.pow(ŋ,4)
+
+    const Bl = Math.cos(B_Rad) * l_Rad
+    const Bl2 = Math.pow(Bl,2)
+    const Bl3 = Math.pow(Bl,3)
+    const Bl4 = Math.pow(Bl,4)
+    const Bl5 = Math.pow(Bl,5)
+    const Bl6 = Math.pow(Bl,6)
+
+    let sin_B = Math.sin(B_Rad)
+    let sin_B2 = Math.pow(sin_B,2)
+    let sin_B4 = Math.pow(sin_B,4)
+    let sin_B6 = Math.pow(sin_B,6)
+    let sin_B8 = Math.pow(sin_B,8)
+
+
+
     const m0 = a * (1 - e * e)
     const m2 = (3 / 2) * e * e * m0
     const m4 = (5 / 4) * e * e * m2
@@ -83,35 +102,34 @@ export const BL2XY = (B,L)=>{ // eslint-disable-line no-unused-vars
     const n6 = (5 / 6) * e * e * n4
     const n8 = (7 / 8) * e * e * n6
 
-    let sin_B = Math.sin(B_Rad)
-    // 子午圈曲率半径
-    const M = m0 + m2 * Math.pow(sin_B,2) + m4 * Math.pow(sin_B,4) + m6 * Math.pow(sin_B,6) + m8 * Math.pow(sin_B,8) // eslint-disable-line no-unused-vars
-    // 卯酉圈曲率半径
-    const N = n0 + n2 * Math.pow(sin_B,2) + n4 * Math.pow(sin_B,4) + n6 * Math.pow(sin_B,6) + n8 * Math.pow(sin_B,8) // eslint-disable-line no-unused-vars
-
     const a0 = m0 + (1/2) * m2 + (3/8) * m4 + (5/16) * m6 + (35/128) * m8
     const a2 = (1/2) * m2 + (1/2) * m4 + (15/32) * m6 + (7/16) * m8
     const a4 = (1/8) * m4 + (3/16) * m6 + (7/32) * m8
     const a6 = (1/32) * m6 + (1/16) * m8
     const a8 = (1/128) * m8
+
+
+    // 子午圈曲率半径
+    // const M = m0 + m2*sin_B2 + m4*sin_B4 + m6*sin_B6 + m8*sin_B8
+    // 卯酉圈曲率半径
+    const N = n0 + n2*sin_B2 + n4*sin_B4 + n6*sin_B6 + n8*sin_B8
     // 子午线弧长
     const X = a0 * B_Rad - (1/2) * a2 * Math.sin(2 * B_Rad) + (1 / 4) * a4 * Math.sin(4 * B_Rad) - (1 / 6) * a6 * Math.sin(6 * B_Rad) + (1 / 8) * a8 * Math.sin(8 * B_Rad)
 
-    const Bl = Math.cos(B_Rad) * l_Rad
+
 
     // 中央经线投影为纵轴(x)
     var x = X +
-            (1 / 2) * N * t * Math.pow(Bl,2) +
-            (1 / 24) * N * t * (5 - Math.pow(t,2) + 9 * Math.pow(ŋ,2) + 4 * Math.pow(ŋ,4)) * Math.pow(Bl,4) +
-            (1 / 720) * N * t * (61 - 58 * Math.pow(t,2) + Math.pow(t,4)) * Math.pow(Bl,6)
+            (1 / 2) * N * t * Bl2 +
+            (1 / 24) * N * t * (5 - t2 + 9 * ŋ2 + 4 * ŋ4) * Bl4 +
+            (1 / 720) * N * t * (61 - 58 * t2 + t4) * Bl6
 
     // 赤道投影为横轴(y)
     // 坐标纵轴西移500公里当作起始轴
     var y = 500000 +
             N * Bl +
-            (1/6) * N * (1 - Math.pow(t,2) + Math.pow(ŋ,2)) * Math.pow(Bl,3) +
-            (1/120) * N * (5 - 18 * Math.pow(t,2) + Math.pow(t,4) + 14 * Math.pow(ŋ,2) - 58 * Math.pow(t,2) * Math.pow(ŋ,2) ) * Math.pow(Bl,5)
-
+            (1/6) * N * (1 - t2 + ŋ2) * Bl3 +
+            (1/120) * N * (5 - 18 * t2 + t4 + 14 * ŋ2 - 58 * t2 * ŋ2 ) * Bl5
 
 
     return {
@@ -120,7 +138,23 @@ export const BL2XY = (B,L)=>{ // eslint-disable-line no-unused-vars
     }
 }
 
-export const XY2BL = (xx,yy)=>{ // eslint-disable-line no-unused-vars
+/**
+ * WKID:4547投影坐标系反算公式
+ * 椭球:         CGCS2000
+ * 投影方法:      高斯-克吕格，3度带投影
+ * 中央经线:      114E
+ * @param xx    横向坐标
+ * @param yy    纵向坐标
+ * @returns {{B: {DD: number, S: number, D: number, M: number}, L: {DD: number, S: number, D: number, M: number}}}
+ * B:经度值，是一个对象,对象属性参考
+ * L:维度值，是一个对象
+ * DD:DD,  以度为单位的角度值
+ * D:D,    度分秒，度的部分值
+ * M:M,    度分秒，分的部分值
+ * S:S     度分秒，秒的部分值
+ * @constructor
+ */
+export const XY2BL = (xx,yy)=>{
     var x = yy
     var y = xx - 500000
     const L0 = 114
@@ -181,11 +215,14 @@ export const XY2BL = (xx,yy)=>{ // eslint-disable-line no-unused-vars
 }
 
 
-
-
 /**
  * 弧度转度分秒
- * @param rad       角度，以弧度为单位
+ * @param rad
+ * @returns {{DD: number, S: number, D: number, M: number}}
+ * DD:DD,  rad转为度为单位
+ * D:D,    rad转为度分秒后，度的部分值
+ * M:M,    rad转为度分秒后，分的部分值
+ * S:S     rad转为度分秒后，秒的部分值
  * @constructor
  */
 function R2DMS (rad){
